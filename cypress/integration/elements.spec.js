87c6ed9b3b1cf37dcd9f3e92b2de7f05875b6352
Lucas Cruz <CruzLucas@JohnDeere.com>
2020-08-27 14:20:06 -0300

    new tests

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,20 +1,26 @@
 /// <reference types = "cypress" />
 
 describe('Work with basic elements', () => {
-    it("Text", () => {
-        cy.visit("https://wcaquino.me/cypress/componentes.html");
 
+    //before -> realizar uma vez antes de todos metodos
+    before(() => {
+        cy.visit("https://wcaquino.me/cypress/componentes.html");
+    })
+
+    //Executa todas as vezes para antes de todos os testes
+    beforeEach(() => {
+        cy.reload;
+    })
+
+    it("Text", () => {
         //cy.get("body").should('contain', 'Cuidado');
 
         //cy.get("span").should('contain', 'Cuidado');
        
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...');
-
     })
 
-    it.only("Text", () => {
-        cy.visit("https://wcaquino.me/cypress/componentes.html");
-        
+    it("Links", () => {
         cy.get('[href="#"]').click();
         cy.get('#resultado').should('have.text', 'Voltou!');
         
@@ -22,6 +28,39 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.not.text', 'Voltou!');
         cy.contains('Voltar').click();
         cy.get('#resultado').should('have.text', 'Voltou!');
+    })
+
+    it("TextFieds", () => {
+        //Para escrever no elemento
+        cy.get('#formNome').type('Cypress Test');
+        cy.get('#formNome').should('have.value', 'Cypress Test');
+    
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea');
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???');
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123');
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay: 100})
+            .should('have.value', 'acerto');
+    })
+
+    it('RadioButton', () => {
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked');
+        
+        cy.get('#formSexoMasc')
+            .should('not.be.checked');
+        
+        cy.get("[name='formSexo']").should('have.length', 2);
 
     })
 })
