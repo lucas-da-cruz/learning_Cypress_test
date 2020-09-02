2beba2733b0369b904aeb47a3f2d525a9e07bf96
lucas-da-cruz <50348175+lucas-da-cruz@users.noreply.github.com>
2020-09-01 21:34:19 -0300

    New tests
    New tests

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -4,14 +4,18 @@ describe('Cypress basics', () => {
     it.only("Should visit a page and assert title", () => {
         cy.visit("https://wcaquino.me/cypress/componentes.html");
         
-        cy.pause();
+        //cy.pause();
 
-        cy.title().should('contain', 'Campo').debug();
+        //cy.title().should('contain', 'Campo').debug();
 
         //Titulo da janela da pagina
         cy.title()
             .should("equal", "Campo de Treinamento")
             .and("contain", "Campo");
+        
+        cy.title().then(title => {
+            console.log(title);
+        })
     })
 
     it("Should find and interact with an element", () => {
--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -42,9 +42,53 @@ describe('Esperas...', () => {
         cy.get('#lista li span').should('contain', 'Item 2');
     })
 
-    it.only('Uso do timeout', () => {
-        cy.get('#buttonDelay').click();
-        cy.get('#novoCampo', { timeout: 1000 }).should('exist');
+    it('Uso do timeout', () => {
+        //cy.get('#buttonDelay').click();
+        //cy.get('#novoCampo', { timeout: 1000 }).should('exist');
+
+        //cy.get('#buttonList').click();
+        //é bom evitar o wait fixo e usar o timeout
+        //cy.wait(5000);
+        //cy.get('#lista li span')
+        //    .should('contain', 'Item 2');
+
+
+        cy.get('#buttonListDOM').click();
+        cy.get('#lista li span', {timeout: 30000})
+            .should('have.length', 1);
+        cy.get('#lista li span', {timeout: 30000})
+            .should('have.length', 2);
     })
 
+    it('Reload', () => {
+        cy.get('#buttonCount')
+            .click()
+            .should('have.value', '1')
+    })
+
+    it.only('Should vs Then', () => {
+        //cy.get('#buttonListDOM').click();
+        /**
+         * O THEN aguarda a listagem ser gerada para ser executado
+         */
+        /*cy.get('#lista li span').then($el => {
+            expect($el).to.have.length(1)
+        })*/
+        /**
+         * O should realiza a busca e fica fazendo verificação
+         */
+        /*cy.get('#lista li span').should($el => {
+            expect($el).to.have.length(1)
+        })*/
+
+        cy.get('#buttonListDOM').then($el => {
+            expect($el).to.have.length(1)
+        }).and('have.id', 'buttonListDOM')
+
+        //Na array function com o should, eu só posso retorna meu parametro de entrada
+        cy.get('#buttonListDOM').then($el => {
+            expect($el).to.have.length(1)
+            return 2
+        }).and('eq', 2).and('not.have.id', 'buttonListDOM')
+    })
 })
